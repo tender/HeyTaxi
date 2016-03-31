d6d8e971673099e65a8029792618fffe5a8f59f8
tender <tender.liou@gmail.com>
2016-03-31 23:50:14 +0800

    google map

--- app/src/main/java/com/ta/heytaxi/MapsActivity.java
+++ app/src/main/java/com/ta/heytaxi/MapsActivity.java
@@ -1,8 +1,21 @@
 package com.ta.heytaxi;
 
-import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
+import android.Manifest;
+import android.content.Context;
+import android.location.Location;
 
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -11,9 +24,16 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback ,GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener,LocationListener {
 
     private GoogleMap mMap;
+    private GoogleApiClient googleApiClient;
+    private LocationRequest locationRequest;
+    private LocationManager locationManager;
+    private android.location.LocationListener locationListener;
+    private Double longitude=0.0 ;
+    private Double latitude=0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +43,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
+        configMapReady();
     }
 
 
@@ -39,12 +59,51 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+//        try {
+//            mMap.setMyLocationEnabled(true);
+//        }catch(SecurityException e){
+//            Log.e("Map","  ",e);
+//        }
+
+        if (!googleApiClient.isConnected()) {
+            googleApiClient.connect();
+        }
 
         // Add a marker in Sydney and move the camera
-        LatLng currentPlace = new LatLng(25.033408, 121.564099);
+        UpdateLocation();
+
+        LatLng currentPlace = new LatLng(getLongitude(), getLatitude());
         mMap.addMarker(new MarkerOptions().position(currentPlace).title("Marker in Taipei"));
-        moveMap(currentPlace);
         mMap.moveCamera(CameraUpdateFactory.newLatLng(currentPlace));
+        moveMap(currentPlace);
+
+    }
+
+    public void configMapReady(){
+        configGoogleApiClient();
+
+    }
+
+    // 建立Google API用戶端物件
+    private synchronized void configGoogleApiClient() {
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+
+    // 建立Location請求物件
+    private void configLocationRequest() {
+        locationRequest = new LocationRequest();
+        // 設定讀取位置資訊的間隔時間為一秒（1000ms）
+        locationRequest.setInterval(1000);
+        // 設定讀取位置資訊最快的間隔時間為一秒（1000ms）
+        locationRequest.setFastestInterval(1000);
+        // 設定優先讀取高精確度的位置資訊（GPS）
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
     }
 
     private void moveMap(LatLng place) {
@@ -57,5 +116,117 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // 使用動畫的效果移動地圖
         mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+
+        // 連線到Google API用戶端
+        if (!googleApiClient.isConnected() ) {
+            googleApiClient.connect();
+        }
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // 移除位置請求服務
+        if (googleApiClient.isConnected()) {
+            //LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient,locationListener);
+
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        // 移除Google API用戶端連線
+        if (googleApiClient.isConnected()) {
+            googleApiClient.disconnect();
+        }
+    }
+
+    private void setUpMapIfNeeded() {
+        if (mMap == null) {
+            mMap = ((SupportMapFragment) getSupportFragmentManager().
+                    findFragmentById(R.id.map)).getMap();
+
+            if (mMap != null) {
+                // 移除地圖設定
+                //setUpMap();
+                //processController();
+            }
+        }
+    }
+
+    public void UpdateLocation() {
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new android.location.LocationListener() {
+            public void onLocationChanged(Location newLocation) {
+                setLongitude(newLocation.getLongitude() * 1000000);
+                setLatitude(newLocation.getLatitude() * 1000000);
+                Log.i("Location=", "X=" + getLongitude().intValue() + ", Y=" + getLatitude().intValue());
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+        };
+        try {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, new Long(0).longValue(), new Float(0).floatValue(), locationListener);
+        }catch(SecurityException e){
+            Log.e("Map","  ",e);
+        }
+
+    }
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
     }
 }
