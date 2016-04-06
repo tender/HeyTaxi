bdf106f273d2acc2c5124a2068a27f8bab0b9942
tenderliou <tender.liou@gmail.com>
2016-04-06 18:15:38 +0800

    google map

--- app/src/main/java/com/ta/heytaxi/MapsActivity.java
+++ app/src/main/java/com/ta/heytaxi/MapsActivity.java
@@ -6,11 +6,14 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 
@@ -35,6 +38,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private android.location.LocationListener locationListener;
     private Double longitude = 0.0;
     private Double latitude = 0.0;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +53,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         configMapReady();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
 
@@ -61,11 +72,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        try {
-            mMap.setMyLocationEnabled(true);
-        }catch(SecurityException e){
-            Log.e("Map","  ",e);
-        }
 
         if (!googleApiClient.isConnected()) {
             googleApiClient.connect();
@@ -149,18 +155,50 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onStart() {
         googleApiClient.connect();
         super.onStart();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Maps Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.ta.heytaxi/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
     }
 
 
     @Override
     protected void onStop() {
         super.onStop();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Maps Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.ta.heytaxi/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
 
         // 移除Google API用戶端連線
         if (googleApiClient.isConnected()) {
             googleApiClient.disconnect();
         }
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.disconnect();
     }
 
     private void setUpMapIfNeeded() {
@@ -201,11 +239,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         };
         try {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, new Long(0).longValue(), new Float(0).floatValue(), locationListener);
-        } catch (SecurityException e) {
-            Log.e("Map", "  ", e);
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, getLongitude().longValue(), getLatitude().floatValue(), locationListener);
+        }catch(SecurityException e){
+            Log.e("Map",e.getMessage());
         }
-
     }
 
     @Override
