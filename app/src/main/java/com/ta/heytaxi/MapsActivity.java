9b0d0dcac639a582846bc692fb9e9c5fd296f8f1
tender <tender.liou@gmail.com>
2016-04-26 22:00:55 +0800

    google map

--- app/src/main/java/com/ta/heytaxi/MapsActivity.java
+++ app/src/main/java/com/ta/heytaxi/MapsActivity.java
@@ -1,11 +1,18 @@
 package com.ta.heytaxi;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.provider.Settings;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.graphics.ColorUtils;
 import android.support.v7.widget.ThemedSpinnerAdapter;
 import android.util.Log;
 import android.view.View;
@@ -22,8 +29,10 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
@@ -50,7 +59,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     ListView orderListView;
     private CustomerOrderAdapter adapter;
     private List<CustomerOrder> items;
-    private List<CustomerOrder> itemsForMap;
+    private List<MarkerOptions> mapMarkers;
+    LatLng currentLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,7 +74,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         orderListView=(ListView) findViewById(R.id.orderlistView);
         items=createOrderItems();
-        itemsForMap = createOrderItems();
+        //itemsForMap = createOrderItems();
         //Collections.copy(itemsForMap, items);
 
         adapter=new CustomerOrderAdapter(items,this);
@@ -72,7 +82,30 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         orderListView.setOnItemClickListener(this);
 
 
+        updateLocation();
 
+    }
+
+    @SuppressWarnings("MissingPermission")
+    protected void updateLocation(){
+
+        // Acquire a reference to the system Location Manager
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        // Define a listener that responds to location updatesF
+        LocationListener locationListener = new OrderLocationListener();
+
+        // Register the listener with the Location Manager to receive location updates
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+
+//        Location location=locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+//        Log.i("current location",location.toString());
+
+    }
+
+    private void makeUseOfNewLocation(Location location){
+        Log.i("current location",String.valueOf(location.getLatitude()));
+        Log.i("current location",String.valueOf(location.getLongitude()));
     }
 
     private List<CustomerOrder> createOrderItems(){
@@ -96,17 +129,32 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     private void putCustomerOrderInfoToMap(List<CustomerOrder> _items){
+        boolean isFirst=true;
+        MarkerOptions marker=null;
         for(CustomerOrder _item:_items){
             Log.i("CustomerOrder",_item.toString());
-            drawMarker(_item.getCurrentLocationByLatLng());
+
+            if(isFirst){
+                drawMarker(_item.getCurrentLocationByLatLng(),isFirst);
+                currentLocation=_item.getCurrentLocationByLatLng();
+                isFirst=false;
+            }else{
+                drawMarker(_item.getCurrentLocationByLatLng(),isFirst);
+            }
+
+            marker=new MarkerOptions();
+            marker.position(_item.getCurrentLocationByLatLng());
+            getMapMarkers().add(marker);
+
         }
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        reloadMarker();
         LatLng value=((CustomerOrder)parent.getAdapter().getItem(position)).getCurrentLocationByLatLng();
         playAnimateCamera(value,3000);
-        //drawMarker(value);
+        drawMarker(value,true);
         moveMap(value);
     }
 
@@ -120,7 +168,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(defaultTaipei).title("Marker in Taipei"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(defaultTaipei));
         //moveMap(defaultTaipei);
-        putCustomerOrderInfoToMap(itemsForMap);
+
+        setMapMarkers(new ArrayList<MarkerOptions>());
+        putCustomerOrderInfoToMap(items);
+
+
     }
 
     @SuppressWarnings("MissingPermission")
@@ -195,6 +247,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     private void drawMarker(LatLng latLng) {
+        drawMarker(latLng,false);
+    }
+    private void drawMarker(LatLng latLng,boolean isCurrentIcon) {
 
         // 1.建立 Marker
         MarkerOptions options = new MarkerOptions(); // 建立標記選項的實例
@@ -203,11 +258,44 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         options.snippet("緯經度:" + latLng); // Info-Window標記摘要
         options.anchor(0.5f, 1.0f); // 錨點
         options.draggable(true); // 是否可以拖曳標記?
+
+        if(isCurrentIcon) {
+            options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+        }
+        //options.
+
         // 2.將Marker加入到地圖中
         mMap.addMarker(options);
         // 3.移動到Marker
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+
+
     }
+
+    private void reloadMarker(){
+        for(MarkerOptions item:getMapMarkers()){
+            drawMarker(item.getPosition());
+        }
+
+    }
+
+
+    public List<CustomerOrder> getItems() {
+        return items;
+    }
+
+    public void setItems(List<CustomerOrder> items) {
+        this.items = items;
+    }
+
+    public List<MarkerOptions> getMapMarkers() {
+        return mapMarkers;
+    }
+
+    public void setMapMarkers(List<MarkerOptions> mapMarkers) {
+        this.mapMarkers = mapMarkers;
+    }
+
     @Override
     protected void onResume() throws SecurityException {
         super.onResume();
@@ -259,402 +347,5 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     private GoogleApiClient client;
 
-/*
-    private Context context;
 
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) throws SecurityException {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
-        setUpMapIfNeeded();
-        context = this;
-
-        GoogleMap m_map = ((SupportMapFragment)
-                getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
-        mMap.setMyLocationEnabled(true);
-
-
-
-        // 設定地圖類型
-        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        // 地圖上顯示建築物
-        // 注意：zoom的設定要 >=17才會顯示建築物
-        mMap.setBuildingsEnabled(true);
-
-        // 顯示目前所在位置
-        mMap.setMyLocationEnabled(true);
-
-        // Google地圖使用者操作界面功能設定
-        UiSettings ui = mMap.getUiSettings();
-        // 開啟/關閉縮放鈕
-        ui.setZoomControlsEnabled(true);
-        // 開啟/關閉地圖捲動手勢
-        ui.setScrollGesturesEnabled(true);
-        // 開啟/關閉地圖縮放手勢
-        ui.setZoomGesturesEnabled(true);
-        // 開啟/關閉地圖傾斜手勢
-        ui.setTiltGesturesEnabled(true);
-        // 開啟/關閉地圖旋轉手勢
-        ui.setRotateGesturesEnabled(true);
-
-        // 按一下地圖即可取得該點經緯度
-        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-            @Override
-            public void onMapClick(LatLng point) {
-                // 根據point取得該經緯度所對應的地址/地標
-                String address = Helper.getAddressByLatLng(point);
-                if (address == null) {
-                    Toast.makeText(context, "Not found !", Toast.LENGTH_SHORT)
-                            .show();
-                } else {
-                    Toast.makeText(context, address, Toast.LENGTH_SHORT).show();
-                    // 演示地圖相機動畫效果
-                    playAnimateCamera(point, 3000);
-                }
-            }
-        });
-    }
-
-
-    private void setUpMapIfNeeded() {
-        // Do a null check to confirm that we have not already instantiated the map.
-        if (mMap == null) {
-            // Try to obtain the map from the SupportMapFragment.
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-                    .getMap();
-            // Check if we were successful in obtaining the map.
-            if (mMap != null) {
-                setUpMap();
-            }
-        }
-    }
-
-    private void playAnimateCamera(LatLng latlng, int durationMs) {
-        // 設置相關地圖相機位置參數，其中zoom的設定要 >=17才會顯示建築物
-        CameraPosition cameraPos = new CameraPosition.Builder().target(latlng)
-                .zoom(17.0f).bearing(300).tilt(45).build();
-        // 定義地圖相機移動
-        CameraUpdate cameraUpt = CameraUpdateFactory
-                .newCameraPosition(cameraPos);
-        // 地圖相機動畫行程設定
-        mMap.animateCamera(cameraUpt, durationMs, null);
-    }
-
-    private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
-    }*/
-
-/*
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-
-        configMapReady();
-
-        setUpMapIfNeeded();
-
-        // 建立Google API用戶端物件
-        configGoogleApiClient();
-
-        // 建立Location請求物件
-        configLocationRequest();
-
-
-        // 連線到Google API用戶端
-        if (!googleApiClient.isConnected()) {
-            googleApiClient.connect();
-        }
-
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        Log.i("Test",String.valueOf(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)));
-        updateLocation();
-    }
-
-
-    private void setMapInfomation() throws SecurityException{
-        // 設定地圖類型
-        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        // 地圖上顯示建築物
-        // 注意：zoom的設定要 >=17才會顯示建築物
-        mMap.setBuildingsEnabled(true);
-
-        // Google地圖使用者操作界面功能設定
-        UiSettings ui = mMap.getUiSettings();
-        // 開啟/關閉縮放鈕
-        ui.setZoomControlsEnabled(true);
-        // 開啟/關閉地圖捲動手勢
-        ui.setScrollGesturesEnabled(true);
-        // 開啟/關閉地圖縮放手勢
-        ui.setZoomGesturesEnabled(true);
-        // 開啟/關閉地圖傾斜手勢
-        ui.setTiltGesturesEnabled(true);
-        // 開啟/關閉地圖旋轉手勢
-        ui.setRotateGesturesEnabled(true);
-
-*/
-/*        // 按一下地圖即可取得該點經緯度
-        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-            @Override
-            public void onMapClick(LatLng point) {
-                // 根據point取得該經緯度所對應的地址/地標
-                String address = Helper.getAddressByLatLng(point);
-                if (address == null) {
-                    Toast.makeText(context, "Not found !", Toast.LENGTH_SHORT)
-                            .show();
-                } else {
-                    Toast.makeText(context, address, Toast.LENGTH_SHORT).show();
-                    // 演示地圖相機動畫效果
-                    playAnimateCamera(point, 3000);
-                }
-            }
-        });*//*
-
-    }
-
-    */
-/**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     *//*
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) throws SecurityException{
-        mMap = googleMap;
-        if (!googleApiClient.isConnected()) {
-            googleApiClient.connect();
-        }
-
-        updateLocation();
-        setMapInfomation();
-        LatLng currentPlace = new LatLng(getLongitude(), getLatitude());
-        mMap.addMarker(new MarkerOptions().position(currentPlace).title("Marker in Taipei"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentPlace));
-        moveMap(currentPlace);
-
-    }
-
-    public void configMapReady() {
-        configGoogleApiClient();
-    }
-
-    // 建立Google API用戶端物件
-    private synchronized void configGoogleApiClient() {
-        googleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
-    }
-
-
-    // 建立Location請求物件
-    private void configLocationRequest() {
-        locationRequest = new LocationRequest();
-        // 設定讀取位置資訊的間隔時間為一秒（1000ms）
-        locationRequest.setInterval(1000);
-        // 設定讀取位置資訊最快的間隔時間為一秒（1000ms）
-        locationRequest.setFastestInterval(1000);
-        // 設定優先讀取高精確度的位置資訊（GPS）
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-
-
-        //locationRequest.setPriority(LocationRequest.PRIORITY_LOW_POWER);
-
-    }
-
-    private void moveMap(LatLng place) {
-        // 建立地圖攝影機的位置物件
-        CameraPosition cameraPosition =
-                new CameraPosition.Builder()
-                        .target(place)
-                        .zoom(17)
-                        .build();
-
-        // 使用動畫的效果移動地圖
-        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-    }
-
-    @Override
-    protected void onResume() throws SecurityException {
-        super.onResume();
-
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 60000, 1, locationListener);
-        setUpMapIfNeeded();
-
-        // 連線到Google API用戶端
-        if (!googleApiClient.isConnected()) {
-            googleApiClient.connect();
-        }
-
-    }
-
-    @Override
-    protected void onPause() throws SecurityException{
-        super.onPause();
-        locationManager.removeUpdates(locationListener);
-        // 移除位置請求服務
-        if (googleApiClient.isConnected()) {
-            LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, this);
-        }
-    }
-
-
-    @Override
-    protected void onStart() {
-
-        super.onStart();
-        googleApiClient.connect();
-    }
-
-
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-
-        // 移除Google API用戶端連線
-        if (googleApiClient.isConnected()) {
-            googleApiClient.disconnect();
-        }
-    }
-
-    private void setUpMapIfNeeded() {
-        if (mMap == null) {
-            mMap = ((SupportMapFragment) getSupportFragmentManager().
-                    findFragmentById(R.id.map)).getMap();
-
-            if (mMap != null) {
-                // 移除地圖設定
-                setUpMap();
-                //processController();
-            }
-        }
-    }
-
-    private void setUpMap(){
-        mMap.addMarker(new MarkerOptions()
-                        .position(new LatLng(0, 0))
-                        .title("Marker")
-        );
-    }
-
-
-    public void updateLocation() throws SecurityException{
-        //locationManager = (LocationManager) getBaseContext().getSystemService(Context.LOCATION_SERVICE);
-        locationListener = new android.location.LocationListener() {
-            public void onLocationChanged(Location newLocation) {
-                setLongitude(newLocation.getLongitude() * 1000000);
-                setLatitude(newLocation.getLatitude() * 1000000);
-                Log.i("Location=", "X=" + getLongitude().intValue() + ", Y=" + getLatitude().intValue());
-            }
-
-            @Override
-            public void onProviderDisabled(String provider) {
-
-            }
-
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String provider) {
-
-            }
-        };
-
-
-        Location location=locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-        if(location==null){
-            setLongitude(25.047924);
-            setLatitude(121.517081);
-        }else{
-            setLongitude(location.getLongitude());
-            setLatitude(location.getLatitude());
-        }
-
-        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, getLongitude().longValue(), getLatitude().floatValue(), locationListener);
-
-    }
-
-    @Override
-    public void onConnected(Bundle bundle) throws SecurityException{
-        // 已經連線到Google Services
-        // 啟動位置更新服務
-        // 位置資訊更新的時候，應用程式會自動呼叫LocationListener.onLocationChanged
-        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, MapsActivity.this);
-
-
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-
-        // 位置改變
-        // Location參數是目前的位置
-//        currentLocation = location;
-        LatLng latLng = new LatLng(
-                location.getLatitude(), location.getLongitude());
-//
-//        // 設定目前位置的標記
-//        if (currentMarker == null) {
-//            currentMarker = mMap.addMarker(new MarkerOptions().position(latLng));
-//        }
-//        else {
-//            currentMarker.setPosition(latLng);
-//        }
-
-        // 移動地圖到目前的位置
-        moveMap(latLng);
-
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        // Google Services連線失敗
-        // ConnectionResult參數是連線失敗的資訊
-        int errorCode = connectionResult.getErrorCode();
-
-        // 裝置沒有安裝Google Play服務
-        if (errorCode == ConnectionResult.SERVICE_MISSING) {
-            Toast.makeText(this, "google_play_service_missing",
-                    Toast.LENGTH_LONG).show();
-        }
-    }
-
-
-    public Double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(Double latitude) {
-        this.latitude = latitude;
-    }
-
-    public Double getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(Double longitude) {
-        this.longitude = longitude;
-    }
-*/
 }
---
+++ app/src/main/java/com/ta/heytaxi/OrderLocationListener.java
@@ -0,0 +1,34 @@
+package com.ta.heytaxi;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+
+/**
+ * Created by IT-0002993 on 16/4/26.
+ */
+public class OrderLocationListener implements LocationListener {
+    public OrderLocationListener(){
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
